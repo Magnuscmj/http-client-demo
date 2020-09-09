177dbce5f4da6e3b59bdef3c8eadcca5aa8d60ea
Magnuscmj <56219481+Magnuscmj@users.noreply.github.com>
2020-09-09 12:34:22 +0200

    2

--- src/main/java/no/kristiania/httpclient/HttpClient.java
+++ src/main/java/no/kristiania/httpclient/HttpClient.java
@@ -6,7 +6,8 @@ import java.net.Socket;
 public class HttpClient {
     public static void main(String[] args) throws IOException {
         Socket socket = new Socket("urlecho.appspot.com", 80);
-        String request = "";
-        socket.getOutputStream().write(request.getBytes());
+        String requestTarget = "/echo?status=200&body=Kristiania";
+        socket.getOutputStream().write(requestTarget.getBytes());
+
     }
 }
